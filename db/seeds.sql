7d8656faaaa2fbedc87be1e42d180cb9c1e95b84
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-03 14:53:33 -0500

    working on seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,23 @@
-INSERT INTO departments (dep_id, dep_name)
-VALUES(01, "Customer Service"),
-      (02, "Maintenence"),
-      (03, "Human Resources"),
-      (04, "Managment"),
+INSERT INTO department (dep_id, dep_name)
+VALUES(1, "Customer Service"),
+      (2, "Maintenence"),
+      (3, "Human Resources"),
+      (4, "Managment");|
 
-INSERT INTO role (role_id, title, salary, department_id)
-VALUES(14, "Manager", 150000, 04),
-      (12, "Custodian", 35000),
-      (15, "Training Coordinator", 80000, 03),
-      (11, "Cashier", 30000, 01),
-      (13, "Klutzy Colleague", 165000, 04),
+INSERT INTO roles (role_id, title, salary, department_id)
+VALUES(14, "Manager", 150000, 4),
+      (12, "Custodian", 35000, 2),
+      (15, "Training Coordinator", 80000, 3),
+      (11, "Cashier", 30000, 1),
+      (13, "Klutzy Colleague", 165000, 4);
 
 INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id) 
-VALUES(001, "Gendo", "Ikari" 13, 001),
-      (002, "Lucy", "Nyu" 14, 001),
-      (003, "Light", "Yagami", 15, 002),
-      (004, "Elizabeth", "Bathory", 11, 003),
-      (005, "Liv", "Moore", 12, 002),
-      (006, "Hannibal", "Lecter", 12, 002),
-      (007, "John", "Kramer", 11, 003),
+VALUES(1, "Gendo", "Ikari" 13, 1),
+      (2, "Lucy", "Nyu" 14, 1),
+      (3, "Light", "Yagami", 15, 2),
+      (4, "Elizabeth", "Bathory", 11, 3),
+      (5, "Liv", "Moore", 12, 2),
+      (6, "Hannibal", "Lecter", 12, 2),
+      (7, "John", "Kramer", 11, 3); 
 
       
