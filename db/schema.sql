4378d81fa2e75c27195cfee5df665a253e83d561
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-04 07:51:33 -0500

    working on getting the adds to work

--- db/schema.sql
+++ db/schema.sql
@@ -7,26 +7,26 @@ USE records_db;
 CREATE TABLE departments (
    department_id INT UNSIGNED AUTO_INCREMENT,
     dep_name VARCHAR(30),
-PRIMARY KEY(dep_id)
+PRIMARY KEY(department_id)
 );
 
 CREATE TABLE roles (
     role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary INT, 
-    deptartment_id INT,
+    department_id INT UNSIGNED,
     -- PRIMARY KEY (role_id),
-   FOREIGN KEY (deptartment_id)
+   FOREIGN KEY (department_id)
    REFERENCES departments(department_id)
    ON DELETE SET NULL
    
 );
 
-CREATE TABLE employees (
+CREATE TABLE employees(
     employee_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
+    role_id INT UNSIGNED,
     salary INT,
     manager_id INT UNSIGNED,
 -- PRIMARY KEY (employee_id),
@@ -34,5 +34,5 @@ FOREIGN KEY (role_id)
 REFERENCES roles(role_id)
 ON DELETE SET NULL,
 FOREIGN KEY (manager_id)
-REFERENCES employee(employee_id)
+REFERENCES employees(employee_id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE records_db;
-INSERT INTO department (dep_id, dep_name)
+INSERT INTO departments (department_id, dep_name)
 VALUES(1, "Customer Service"),
       (2, "Maintenence"),
       (3, "Human Resources"),
@@ -11,7 +11,7 @@ VALUES  (12, "Custodian", 35000, 2),       (15, "Training Coordinator", 80000, 3
       (13, "Klutzy Colleague", 165000, 4),
       (14,"Manager",150000,4);
 
-INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id) 
+INSERT INTO employees (employee_id, first_name, last_name, role_id, manager_id) 
 VALUES(1, "Gendo", "Ikari", 13, 1),
       (2, "Lucy", "Nyu" ,14, 1),
       (3, "Light", "Yagami", 15, 2),
